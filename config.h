07545ff29b890ba04b715a374650ae02dec2154b
= <=>
2022-12-23 22:03:02 -0300

    con alarmas

---
+++ config.h
@@ -0,0 +1,61 @@
+
+  int iniciobarras= 54;
+  int anchobarras= 74;
+  int altobarras= 13;
+
+// ALARMAS
+  #define ESPERA_EN_CONTACTO 2000
+
+  #define TEMP_MIN_BLOCK  60 
+  #define TEMP_ALERTA_BLOCK  105
+  #define TEMP_MAX_BLOCK  110
+  
+  #define TEMP_MIN_AGUA  60
+  #define TEMP_ALERTA_AGUA 95
+  #define TEMP_MAX_AGUA  100
+
+  #define PRES_MIN_ACEITE  5
+  #define PRES_ALERTA_MIN_ACEITE 10 
+  #define PRES_ALERTA_MAX_ACEITE  120
+  #define PRES_MAX_ACEITE 125
+
+  #define VOLTAJE_MEDIO_BAT  12
+  #define VOLTAJE_MIN_BAT  8
+  #define VOLTAJE_ALERTA_MIN_BAT  11
+  #define VOLTAJE_ALERTA_MAX_BAT  15
+  #define VOLTAJE_MAX_BAT  16 
+
+  #define INTERVALO_BUZZER_ALARMA 10000 // long millis
+  #define INTERVALO_BUZZER_PARAR 0 // long millis
+  #define DUARACION_BUZZER_ALARMA 2000 // long millis
+
+
+  // divisores de voltaje
+  #define R1_BAT  4700 //+12-Vout 4639- 3675
+  #define R2_BAT  983 // Vout-GND
+  #define R1_TEMP  32.8 // Vout-GND
+  #define R1_PRES 33.0 // Vout-GND
+  #define VTOTAL = 5 // voltaje total
+
+  //coef Steinhart-Hart para bulbo de temp SIEMENS AMR1425 (original)
+  #define A_TEMP  0.00174810878912309
+  #define B_TEMP  0.000252033176331
+  #define C_TEMP  6.23E-08
+
+  //coef Bulbo Presion de Aceite Orland Rober
+  // lo calcule acá: https://docs.google.com/spreadsheets/d/1qfQCLduFSSs-z2fxA71gRAYeHIXaSkvhZlhe-7v67Lw/edit#gid=857753747
+  #define A_PRES  0.87
+  #define B_PRES  6.35 //- en la ecuacion
+
+  // PINES
+  #define AIN_BAT  A0
+  #define AIN_TEMP_REGRIG  A1
+  #define AIN_PRESION  A2
+  #define PIN_TERMOMETRO 3
+  #define PIN_BUZZER 13 // 6
+  #define PIN_SENSOR_NIVEL_REF 4
+  #define PIN_LATIDO 6 // 13
+  
+  
+
+
---
+++ dibujar_alarmas.ino
@@ -0,0 +1,196 @@
+void dibujarAlarmaTempBlock(int valorMedido){
+  display.fillRect(0, 0, 128, altobarras+2, WHITE);
+  display.setFont(); // usa fuente default
+  display.setTextColor(BLACK);
+  display.setCursor(1, 4);
+  display.print("BLK: ");
+  display.setCursor(24, 4);
+  display.print(TEMP_ALERTA_BLOCK);
+  display.print("<");
+  display.setCursor(84, 4);
+  display.print(">");
+  display.print(TEMP_MAX_BLOCK);
+  display.print("(!)");
+  display.setFont(&FreeSans9pt7b);
+  display.setCursor(50,13);
+  if (valorMedido<100) {
+    display.print("  ");
+  }
+  display.print(valorMedido);
+}
+
+void dibujarAlarmaTempAgua(int valorMedido){
+  display.fillRect(0, 16, 128, altobarras+2, WHITE);
+  display.setFont(); // usa fuente default
+  display.setTextColor(BLACK);
+  display.setCursor(1, 20);
+  display.print("RFR: ");
+  display.setCursor(28, 20);
+  display.print(TEMP_ALERTA_AGUA);
+  display.print("<");
+  display.setCursor(84, 20);
+  display.print(">");
+  display.print(TEMP_MAX_AGUA);
+  display.print("(!)");
+  display.setFont(&FreeSans9pt7b);
+  display.setCursor(50,29);
+  if (valorMedido<100) {
+    display.print("  ");
+  }
+  display.print(valorMedido);
+}
+
+void dibujarAlarmaMinPresionAceite(int valorMedido){
+  display.fillRect(0, 32, 128, altobarras+2, WHITE);
+  display.setFont(); // usa fuente default
+  display.setTextColor(BLACK);
+  display.setCursor(1, 36);
+  display.print("PRE: ");
+  display.setCursor(38, 36);
+  display.print(PRES_MIN_ACEITE);
+  display.print(" >");
+  display.setCursor(84, 36);
+  display.print(" > ");
+  display.print(PRES_MIN_ACEITE);
+  display.print("(!)");
+  display.setFont(&FreeSans9pt7b);
+  display.setCursor(60,45);
+  if (valorMedido<100) {
+    display.print("  ");
+  }
+  display.print(valorMedido); //valorMedido  
+}
+
+void dibujarAlarmaMaxPresionAceite(int valorMedido){
+  display.fillRect(0, 32, 128, altobarras+2, WHITE);
+  display.setFont(); // usa fuente default
+  display.setTextColor(BLACK);
+  display.setCursor(1, 36);
+  display.print("PRE: ");
+  display.setCursor(25, 36);
+  display.print(PRES_ALERTA_MAX_ACEITE);
+  display.print("<");
+  display.setCursor(83, 36);
+  display.print("<");
+  display.print(PRES_MAX_ACEITE);
+  display.print("(!)");
+  display.setFont(&FreeSans9pt7b);
+  display.setCursor(50,45);
+  if (valorMedido<100) {
+    display.print("  ");
+  }
+  display.print(valorMedido); //valorMedido  
+}
+
+void dibujarPararTempBlock(int valorMedido){
+  display.fillRect(0, 0, 128, 64, WHITE);
+  display.setTextColor(BLACK);
+  display.setFont(); // usa fuente default
+  display.setCursor(35, 25);
+  display.print("TEMP BLOCK");
+  display.setCursor(28, 53);
+  display.print("MAYOR QUE ");
+  display.print(TEMP_MAX_BLOCK);
+  display.setFont(&FreeSans9pt7b);
+  display.setCursor(33,20);
+  display.print("PARAR");
+  display.setCursor(47,48);
+  display.print(valorMedido); //valorMedido  
+}
+
+void dibujarPararTempAgua(int valorMedido){
+  display.fillRect(0, 0, 128, 64, WHITE);
+  display.setTextColor(BLACK);
+  display.setFont(); // usa fuente default
+  display.setCursor(10, 25);
+  display.print("TEMP REFRIGERANTE");
+  display.setCursor(28, 53);
+  display.print("MAYOR QUE ");
+  display.print(TEMP_MAX_AGUA);
+  display.setFont(&FreeSans9pt7b);
+  display.setCursor(33,20);
+  display.print("PARAR");
+  display.setCursor(47,48);
+  display.print(valorMedido); //valorMedido  
+}
+
+void dibujarPararMinAceite(int valorMedido){
+  display.fillRect(0, 0, 128, 64, WHITE);
+  display.setTextColor(BLACK);
+  display.setFont(); // usa fuente default
+  display.setCursor(13, 25);
+  display.print("PRESION DE ACEITE");
+  display.setCursor(28, 53);
+  display.print("MENOR QUE ");
+  display.print(PRES_MIN_ACEITE);
+  display.setFont(&FreeSans9pt7b);
+  display.setCursor(33,20);
+  display.print("PARAR");
+  display.setCursor(60,48);
+  display.print(valorMedido); //valorMedido  
+}
+
+void dibujarPararMaxAceite(int valorMedido){
+  display.fillRect(0, 0, 128, 64, WHITE);
+  display.setTextColor(BLACK);
+  display.setFont(); // usa fuente default
+  display.setCursor(13, 25);
+  display.print("PRESION DE ACEITE");
+  display.setCursor(28, 53);
+  display.print("MAYOR QUE ");
+  display.print(PRES_MAX_ACEITE);
+  display.setFont(&FreeSans9pt7b);
+  display.setCursor(33,20);
+  display.print("PARAR");
+  display.setCursor(50,48);
+  display.print(valorMedido); //valorMedido  
+}
+
+void dibujarPararMinBateria(int valorMedido){
+  display.fillRect(0, 0, 128, 64, WHITE);
+  display.setTextColor(BLACK);
+  display.setFont(); // usa fuente default
+  display.setCursor(13, 25);
+  display.print("VOLTAJE BATERIA");
+  display.setCursor(28, 53);
+  display.print("MENOR QUE ");
+  display.print(VOLTAJE_MIN_BAT);
+  display.print("v");
+  display.setFont(&FreeSans9pt7b);
+  display.setCursor(33,20);
+  display.print("PARAR");
+  display.setCursor(53,48);
+  display.print(valorMedido); //valorMedido  
+  display.print("v");
+}
+
+void dibujarPararMaxBateria(int valorMedido){
+  display.fillRect(0, 0, 128, 64, WHITE);
+  display.setTextColor(BLACK);
+  display.setFont(); // usa fuente default
+  display.setCursor(22, 25);
+  display.print("VOLTAJE BATERIA");
+  display.setCursor(25, 53);
+  display.print("MAYOR QUE ");
+  display.print(VOLTAJE_MAX_BAT);
+  display.print("v");
+  display.setFont(&FreeSans9pt7b);
+  display.setCursor(33,20);
+  display.print("PARAR");
+  display.setCursor(49,48);
+  display.print(valorMedido); //valorMedido  
+  display.print("v");
+}
+
+void dibujarPararNivelBajoAgua(){
+  display.fillRect(0, 0, 128, 64, WHITE);
+  display.setTextColor(BLACK);
+  display.setFont(); // usa fuente default
+  display.setCursor(10, 27);
+  display.print("NIVEL REFRIGERANTE");
+  display.setFont(&FreeSans9pt7b);
+  display.setCursor(33,20);
+  display.print("PARAR");
+  display.setCursor(45,54);
+  display.print("BAJO"); //valorMedido  
+}
